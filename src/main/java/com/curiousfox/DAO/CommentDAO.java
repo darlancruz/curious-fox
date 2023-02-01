fc12c9452235b46eb80926e9a7959371da797b41
darlan <darlan.silvadacruz@gmail.com>
2023-02-01 13:08:59 -0300

    create comment dao

---
+++ src/main/java/com/curiousfox/DAO/CommentDAO.java
@@ -0,0 +1,34 @@
+package com.curiousfox.DAO;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.sql.Types;
+
+import com.curiousfox.jdbc.ConnectionFactory;
+import com.curiousfox.model.Comment;
+
+public class CommentDAO {
+	private Connection connection;
+	
+	public CommentDAO(){
+		this.connection = new ConnectionFactory().getConnection();
+	}
+	
+	public void addComment(Comment comment) {
+		String sql = "INSERT INTO comments (sender_id, receiver_id, comment_text, created_at) values (?,?,?,?)";
+		try {
+			PreparedStatement stmt = connection.prepareStatement(sql);
+			stmt.setObject(1, comment.getSender_id(), Types.OTHER);
+			stmt.setObject(2, comment.getReceiver_id(), Types.OTHER);
+			stmt.setString(3, comment.getText());
+			stmt.setTimestamp(4, Timestamp.from(comment.getCreated_at()));
+			
+			stmt.execute();
+			stmt.close();
+		} catch(SQLException e) {
+			throw new RuntimeException(e);
+		}	
+	}
+}
