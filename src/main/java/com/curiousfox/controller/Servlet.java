c59fcb4f00cc13132dd8f5ffa5ff0fda54e5ab12
darlan <darlan.silvadacruz@gmail.com>
2023-02-02 10:53:19 -0300

    send comments to the database

--- src/main/java/com/curiousfox/controller/Servlet.java
+++ src/main/java/com/curiousfox/controller/Servlet.java
@@ -1,6 +1,7 @@
 package com.curiousfox.controller;
 
 import java.io.IOException;
+import java.time.Instant;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -9,10 +10,12 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.curiousfox.DAO.CommentDAO;
 import com.curiousfox.DAO.UserDAO;
+import com.curiousfox.model.Comment;
 import com.curiousfox.model.User;
 
-@WebServlet(urlPatterns = {"/profile"})
+@WebServlet(urlPatterns = {"/profile", "/send"})
 public class Servlet extends HttpServlet {
 	/** The Constant serialVersionUID. */
 	private static final long serialVersionUID = 1L;
@@ -23,6 +26,17 @@ public class Servlet extends HttpServlet {
 		String action = req.getServletPath();
 		if (action.equals("/profile")) {
 			getUser(req, res);
+		}else {
+			res.sendRedirect("404");
+		}
+	}
+	
+	protected void doPost(HttpServletRequest req, HttpServletResponse res)
+			throws ServletException, IOException {
+		
+		String action = req.getServletPath();
+		if (action.equals("/send")) {
+			sendComment(req, res);
 		}else {
 			res.sendRedirect("404");
 		}
@@ -41,5 +55,21 @@ public class Servlet extends HttpServlet {
 		
 		RequestDispatcher rd = req.getRequestDispatcher("profile.jsp");
 		rd.forward(req, res);
+	}
+	
+	protected void sendComment(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		String receiverId = req.getParameter("receiver_id");
+		String receiverUsername = req.getParameter("receiver_username");
+		String text = req.getParameter("text");
+		
+		Comment comment = new Comment();
+		comment.setReceiver_id(receiverId);
+		comment.setText(text);
+		comment.setCreated_at(Instant.now());
+		
+		CommentDAO dao = new CommentDAO();
+		dao.addComment(comment);
+		
+		res.sendRedirect(req.getContextPath() + "/profile?username="+receiverUsername);
 	}
 }
