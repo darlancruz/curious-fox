49bca92fd0d02f50fd6dfb689ae8347e9129178d
darlan <darlan.silvadacruz@gmail.com>
2023-02-11 23:38:00 -0300

    (UserDAO) add method to create user

--- src/main/java/com/curiousfox/DAO/UserDAO.java
+++ src/main/java/com/curiousfox/DAO/UserDAO.java
@@ -8,6 +8,7 @@ import java.sql.Types;
 
 import com.curiousfox.jdbc.ConnectionFactory;
 import com.curiousfox.model.User;
+import com.curiousfox.utils.DiceBearUrlGenerator;
 
 public class UserDAO {
 	private Connection connection;
@@ -67,6 +68,23 @@ public class UserDAO {
 			throw new RuntimeException(e);
 		}
 	}
+	
+	public void createUser(User user) {
+		String sql = "INSERT INTO accounts (name, username, password, picture_url) VALUES (?,?,?,?)";
+		try {
+			PreparedStatement stmt = connection.prepareStatement(sql);
+			stmt.setString(1, user.getName());
+			stmt.setString(2, user.getUsername());
+			stmt.setString(3, user.getPassword());
+			stmt.setString(4, DiceBearUrlGenerator.getUrl());
+			
+			stmt.execute();
+			stmt.close();
+		}catch(SQLException e) {
+			throw new RuntimeException(e);
+		}
+		
+	} 
 	
 	public boolean checkUserExists(String username) {
 		boolean userExists;
